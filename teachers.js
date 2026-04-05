f2bfa174365240e8b5765872432efe19b2b05a08
ffstreamer204-byte <ffstreamer204@gmail.com>
2026-04-05 22:50:57 +0530

    Add files via upload

---
+++ teachers.js
@@ -0,0 +1,61 @@
+const teachers = {
+  t1: { name: "CHAIRMAN<br>MR. SANDEEP SINGH", photo: "images/t1.jpeg" },
+  t2: { name: "MD<br>MRS. KANIKA SINGH", photo: "images/t2.jpeg" },
+  t3: { name: "ED<br>MR. AMRESH KUMAR SINGH", photo: "images/t3.jpeg" },
+
+  t4: { name: "DEAN, ACADEMICS<br>DR. ABHISHEK SWAMI", photo: "images/t4.jpeg" },
+  t5: { name: "HOD<br>DR. DEEPTI SAXENA", photo: "images/t5.jpeg" },
+
+  t6: { name: "PROFESSOR<br>DR. RAJEEV BHATNAGAR", photo: "images/t6.jpeg" },
+  t7: { name: "TEACHER<br>MS. SHEELA KUSHWAHA", photo: "images/t7.jpeg" },
+  t8: { name: "TEACHER<br>MS. MAMATA SINGH", photo: "images/t8.jpeg" },
+  t9: { name: "TEACHER<br>MS. NIDHI AGRAWAL", photo: "images/t9.jpeg" },
+  t10:{ name: "TEACHER<br>MS. VINITA KUMARI", photo: "images/t10.jpeg" },
+
+  t11:{ name: "HOD<br>MS. MANPREET KAUR", photo: "images/t11.jpeg" },
+  t12:{ name: "TEACHER<br>MS. ANUPRIYA", photo: "images/t12.jpeg" },
+  t13:{ name: "TEACHER<br>MR. VIPUL CHADDHA", photo: "images/t13.jpeg" },
+  t14:{ name: "TEACHER<br>MS. PRAGYA GOYAL", photo: "images/t14.jpeg" },
+  t15:{ name: "TEACHER<br>MS. DARSHIKA TOMAR", photo: "images/t15.jpeg" },
+  t16:{ name: "TEACHER<br>MS. UPASANA VERMA", photo: "images/t16.jpeg" },
+
+  t17:{ name: "PROFESSOR<br>DR. PRAGYA AGARWAL", photo: "images/t17.jpeg" },
+  t18:{ name: "ADMIN OFFICER<br>MR. SUNIL KR. SINGH", photo: "images/t18.jpeg" },
+  t19:{ name: "HR MANAGER<br>MS. KUSUM SHUKLA", photo: "images/t19.jpeg" },
+  t20:{ name: "IMO<br>MR. BHAIRAV KR. JHA", photo: "images/t20.jpeg" },
+  t21:{ name: "IT ADMIN<br>MR. DEVENDRA SHARMA", photo: "images/t21.jpeg" },
+  t22:{ name: "ACCOUNTANT<br>MR. ASIF", photo: "images/t22.jpeg" },
+  t23:{ name: "ACCOUNTANT<br>MR. VINEET", photo: "images/t23.jpeg" },
+
+  t24:{ name: "GRAPHIC DESIGNER<br>MR. KAMRAN", photo: "images/t24.jpeg" },
+  t25:{ name: "FRONT DESK EXECUTIVE<br>MR. ANISHA", photo: "images/t25.jpeg" },
+  t26:{ name: "PLACEMENT OFFICER<br>MS. SHALINI", photo: "images/t26.jpeg" },
+  t27:{ name: "ADMISSION COUNSELLOR<br>MS. NIKITA", photo: "images/t27.jpeg" },
+  t28:{ name: "ADMISSION COUNSELLOR<br>MS. KASHISH", photo: "images/t28.jpeg" },
+
+  t29:{ name: "STAFF<br>MR. ANKIT NAGAR", photo: "images/t29.jpeg" },
+  t30:{ name: "STAFF<br>MS. BHARTI SHARMA", photo: "images/t30.jpeg" },
+
+  t31:{ name: "HOD<br>DR. ATEEQ AHMAD", photo: "images/t31.jpeg" },
+  t32:{ name: "TEACHER<br>MR. JABIR HUSSAIN", photo: "images/t32.jpeg" },
+  t33:{ name: "TEACHER<br>MR. YASHPAL SINGH", photo: "images/t33.jpeg" },
+  t34:{ name: "TEACHER<br>MR. ASHUTOSH MISHRA", photo: "images/t34.jpeg" },
+  t35:{ name: "TEACHER<br>MS. NEETA GUPTA", photo: "images/t35.jpeg" },
+  t36:{ name: "TEACHER<br>MS. KARUNLATA SHUKLA", photo: "images/t36.jpeg" },
+
+  t37:{ name: "HOD<br>MS. IKRA KHAN", photo: "images/t37.jpeg" },
+  t38:{ name: "TEACHER<br>MR. AYUSH WADHAWAN", photo: "images/t38.jpeg" },
+  t39:{ name: "TEACHER<br>MS. SHWETA SAXENA", photo: "images/t39.jpeg" },
+  t40:{ name: "TEACHER<br>MS. SHIKHA BHATNAGAR", photo: "images/t40.jpeg" },
+  t41:{ name: "TEACHER<br>MS. BHARATI", photo: "images/t41.jpeg" },
+  t42:{ name: "TEACHER<br>MS. GURLEEN KAUR", photo: "images/t42.jpeg" },
+  t43:{ name: "PROFESSOR<br>DR. AISWARIYA CHANDEL", photo: "images/t43.jpeg" },
+  t44:{ name: "PROFESSOR<br>DR. PAYAL", photo: "images/t44.jpeg" },
+
+  t45:{ name: "TEACHER<br>MS. RABITA KUMARI", photo: "images/t45.jpeg" },
+  t46:{ name: "TEACHER<br>MR. ANWAR ANSARI", photo: "images/t46.jpeg" },
+  t47:{ name: "TEACHER<br>MR. AKSHIT SINGH BORA", photo: "images/t47.jpeg" },
+  t48:{ name: "TEACHER<br>MR. ASHUTOSH KUMAR", photo: "images/t48.jpeg" },
+
+  t49:{ name: "DISCIPLINE HEAD<br>MR. KC SHARMA", photo: "images/t49.jpeg" }
+};
