b11e3eb90e6cd9aca94cd49cc7df48a56b88c5ad
ffstreamer204-byte <ffstreamer204@gmail.com>
2026-04-06 13:40:44 +0530

    Add files via upload

---
+++ teachers.js
@@ -0,0 +1,73 @@
+const teachers = {
+
+  // TOP TOP MANAGEMENT
+  t1:{ name:"CHAIRMAN<br>MR. SANDEEP SINGH", photo:"images/t1.jpeg", dept:"TOP MANAGEMENT" },
+  t2:{ name:"MD<br>MRS. KANIKA SINGH", photo:"images/t2.jpeg", dept:"TOP MANAGEMENT" },
+  t3:{ name:"ED<br>MR. AMRESH KUMAR SINGH", photo:"images/t3.jpeg", dept:"TOP MANAGEMENT" },
+
+  // ACADEMICS
+  t4:{ name:"DEAN, ACADEMICS<br>DR. ABHISHEK SWAMI", photo:"images/t4.jpeg", dept:"ACADEMICS" },
+
+  // SCIENCE 
+  t5:{ name:"HOD<br>DR. DEEPTI SAXENA", photo:"images/t5.jpeg", dept:"SCIENCE & PHARMACY" },
+  t6:{ name:"PROFESSOR<br>DR. RAJEEV BHATNAGAR", photo:"images/t6.jpeg", dept:"SCIENCE & PHARMACY" },
+  t7:{ name:"TEACHER<br>MS. SHEELA KUSHWAHA", photo:"images/t7.jpeg", dept:"SCIENCE & PHARMACY" },
+  t8:{ name:"TEACHER<br>MS. MAMATA SINGH", photo:"images/t8.jpeg", dept:"SCIENCE & PHARMACY" },
+  t9:{ name:"TEACHER<br>MS. NIDHI AGRAWAL", photo:"images/t9.jpeg", dept:"SCIENCE & PHARMACY" },
+  t10:{ name:"TEACHER<br>MS. VINITA KUMARI", photo:"images/t10.jpeg", dept:"SCIENCE & PHARMACY" },
+
+  // HUMANITIES AND LAW
+  t11:{ name:"HOD<br>MS. MANPREET KAUR", photo:"images/t11.jpeg", dept:"HUMANITIES AND LAWS" },
+  t12:{ name:"TEACHER<br>MS. ANUPRIYA", photo:"images/t12.jpeg", dept:"HUMANITIES AND LAWS" },
+  t13:{ name:"TEACHER<br>MR. VIPUL CHADDHA", photo:"images/t13.jpeg", dept:"HUMANITIES AND LAWS" },
+  t14:{ name:"TEACHER<br>MS. PRAGYA GOYAL", photo:"images/t14.jpeg", dept:"HUMANITIES AND LAWS" },
+  t15:{ name:"TEACHER<br>MS. DARSHIKA TOMAR", photo:"images/t15.jpeg", dept:"HUMANITIES AND LAWS" },
+  t16:{ name:"TEACHER<br>MS. UPASANA VERMA", photo:"images/t16.jpeg", dept:"HUMANITIES AND LAWS" },
+
+  // ADMIN
+  t17:{ name:"PROFESSOR<br>DR. PRAGYA AGARWAL", photo:"images/t17.jpeg", dept:"ADMIN" },
+  t18:{ name:"ADMIN OFFICER<br>MR. SUNIL KR. SINGH", photo:"images/t18.jpeg", dept:"ADMIN" },
+  t19:{ name:"HR MANAGER<br>MS. KUSUM SHUKLA", photo:"images/t19.jpeg", dept:"ADMIN" },
+  t20:{ name:"IMO<br>MR. BHAIRAV KR. JHA", photo:"images/t20.jpeg", dept:"ADMIN" },
+  t21:{ name:"SCIENCE & PHARMACY ADMIN<br>MR. DEVENDRA SHARMA", photo:"images/t21.jpeg", dept:"ADMIN" },
+  t22:{ name:"ACCOUNTANT<br>MR. ASIF", photo:"images/t22.jpeg", dept:"ADMIN" },
+  t23:{ name:"ACCOUNTANT<br>MR. VINEET", photo:"images/t23.jpeg", dept:"ADMIN" },
+
+  // SUPPORT / STAFF
+  t24:{ name:"GRAPHIC DESIGNER<br>MR. KAMRAN", photo:"images/t24.jpeg", dept:"SUPPORT" },
+  t25:{ name:"FRONT DESK EXECUTIVE<br>MR. ANISHA", photo:"images/t25.jpeg", dept:"SUPPORT" },
+  t26:{ name:"PLACEMENT OFFICER<br>MS. SHALINI", photo:"images/t26.jpeg", dept:"SUPPORT" },
+  t27:{ name:"ADMISSION COUNSELLOR<br>MS. NIKSCIENCE & PHARMACYA", photo:"images/t27.jpeg", dept:"SUPPORT" },
+  t28:{ name:"ADMISSION COUNSELLOR<br>MS. KASHISH", photo:"images/t28.jpeg", dept:"SUPPORT" },
+
+  t29:{ name:"STAFF<br>MR. ANKSCIENCE & PHARMACY NAGAR", photo:"images/t29.jpeg", dept:"STAFF" },
+  t30:{ name:"STAFF<br>MS. BHARTI SHARMA", photo:"images/t30.jpeg", dept:"STAFF" },
+
+  //CS AND IT
+  t31:{ name:"HOD<br>DR. ATEEQ AHMAD", photo:"images/t31.jpeg", dept:"CS AND IT" },
+  t32:{ name:"TEACHER<br>MR. JABIR HUSSAIN", photo:"images/t32.jpeg", dept:"CS AND IT" },
+  t33:{ name:"TEACHER<br>MR. YASHPAL SINGH", photo:"images/t33.jpeg", dept:"CS AND IT" },
+  t34:{ name:"TEACHER<br>MR. ASHUTOSH MISHRA", photo:"images/t34.jpeg", dept:"CS AND IT" },
+  t35:{ name:"TEACHER<br>MS. NEETA GUPTA", photo:"images/t35.jpeg", dept:"CS AND IT" },
+  t36:{ name:"TEACHER<br>MS. KARUNLATA SHUKLA", photo:"images/t36.jpeg", dept:"CS AND IT" },
+
+// MANAGEMENTS AND COMMERCE
+  t37:{ name:"HOD<br>MS. IKRA KHAN", photo:"images/t37.jpeg", dept:"MANAGEMENTS AND COMMERCE" },
+  t38:{ name:"TEACHER<br>MR. AYUSH WADHAWAN", photo:"images/t38.jpeg", dept:"MANAGEMENTS AND COMMERCE" },
+  t39:{ name:"TEACHER<br>MS. SHWETA SAXENA", photo:"images/t39.jpeg", dept:"MANAGEMENTS AND COMMERCE" },
+  t40:{ name:"TEACHER<br>MS. SHIKHA BHATNAGAR", photo:"images/t40.jpeg", dept:"MANAGEMENTS AND COMMERCE" },
+  t41:{ name:"TEACHER<br>MS. BHARATI", photo:"images/t41.jpeg", dept:"MANAGEMENTS AND COMMERCE" },
+  t42:{ name:"TEACHER<br>MS. GURLEEN KAUR", photo:"images/t42.jpeg", dept:"MANAGEMENTS AND COMMERCE" },
+  t43:{ name:"PROFESSOR<br>DR. AISWARIYA CHANDEL", photo:"images/t43.jpeg", dept:"MANAGEMENTS AND COMMERCE" },
+  t44:{ name:"PROFESSOR<br>DR. PAYAL", photo:"images/t44.jpeg", dept:"MANAGEMENTS AND COMMERCE" },
+
+
+// MASS AND MEDIA
+  t45:{ name:"TEACHER<br>MS. RABITA KUMARI", photo:"images/t45.jpeg", dept:"MASS AND MEDIA" },
+  t46:{ name:"TEACHER<br>MR. ANWAR ANSARI", photo:"images/t46.jpeg", dept:"MASS AND MEDIA" },
+  t47:{ name:"TEACHER<br>MR. AKSHIT SINGH BORA", photo:"images/t47.jpeg", dept:"MASS AND MEDIA" },
+  t48:{ name:"TEACHER<br>MR. ASHUTOSH KUMAR", photo:"images/t48.jpeg", dept:"MASS AND MEDIA" },
+
+  t49:{ name:"DISCIPLINE HEAD<br>MR. KC SHARMA", photo:"images/t49.jpeg", dept:"ADMIN" }
+
+};
